05c8eab5fb52b9bd041fa51f5fef61791804f5d3
ekaterinatrukhan <eetrukhan@edu.hse.ru>
2021-04-09 00:42:26 +0300

    Final commit

--- Assets/Scripts/CircleMovement.cs
+++ Assets/Scripts/CircleMovement.cs
@@ -5,27 +5,49 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using Slider = UnityEngine.UI.Slider;
 
+/// <summary>
+/// Moves circle by taking coordinates of clicking on plane
+/// </summary>
 public class CircleMovement : MonoBehaviour
 {
-    private float speed;
-    private Vector3 point;
-    private Vector3 mouseTap;
+    // Speed of circle
+    private float speed; 
+    
+    // Coordinates in world
+    private Vector3 point; 
+    
+    // Coordinates of mouseTap or touch of the screen
+    private Vector3 mouseTap; 
+    
+    // Queue of coordinates
     private Queue<Vector3> Points = new Queue<Vector3>(); 
     
+    // Main camera
     [SerializeField]
-    private Camera main_camera;
+    private Camera main_camera; 
     
+    // Object which is going to move
     [SerializeField]
-    private GameObject moveObj;
+    private GameObject moveObj; 
 
+    // Speed controlling slider
     [SerializeField] 
     private Slider slider;
     
+    /// <summary>
+    /// Assigns the starting speed value
+    /// </summary>
     private void Start()
     {
         speed = slider.value;
     }
 
+    /// <summary>
+    /// Checks each frame if there are any points in queue
+    /// If yes, moves the circle towards earliest point
+    /// until circle reaches it. 
+    /// Then throws out reached point and repeat
+    /// </summary>
     void Update()
     {
         if (Points.Any())
@@ -38,11 +60,21 @@ public class CircleMovement : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// When the position of the slider changes,
+    /// the variable speed changes
+    /// </summary>
     public void SpeedChange()
     {
         speed = slider.value;   
     }
  
+    /// <summary>
+    /// Takes coordinates of tap,
+    /// Translates them to local system
+    /// Assigns axle z equal to 0, because we are in 2d
+    /// Places the point in the queue
+    /// </summary>
     void OnMouseDown()
     {
         mouseTap = Input.mousePosition;
